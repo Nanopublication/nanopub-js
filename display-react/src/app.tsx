b44abd876d62186d77ac1e6094868cd909389e3a
Vincent Emonet <vincent.emonet@gmail.com>
2023-03-20 12:33:09 +0100

    fmt

--- display-react/src/app.tsx
+++ display-react/src/app.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react'
-import { createRoot } from 'react-dom/client';
+import {createRoot} from 'react-dom/client'
 import {NanopubDisplayReact} from './NanopubDisplay'
 // import '@nanopub/display'
 // import '../../nanopub-display/src/index'
 
 export default function App() {
-    return <div>
+  return (
+    <div>
       <h1>Nanopub Display React development</h1>
       <NanopubDisplayReact url="https://purl.org/np/RAHtkscyyyJDLvWRuINckQrn5rbHzQKvwakNVC3fmRzGU" />
       {/* <nanopub-display url="https://purl.org/np/RAHtkscyyyJDLvWRuINckQrn5rbHzQKvwakNVC3fmRzGU" /> */}
     </div>
+  )
 }
 
-const root = createRoot(document.getElementById('root') as Element);
-root.render(<App />);
+const root = createRoot(document.getElementById('root') as Element)
+root.render(<App />)
