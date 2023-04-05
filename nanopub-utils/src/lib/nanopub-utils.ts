7b5f062d343eb81f899cb5fa33c6ca3f0222e740
Tobias Kuhn <kuhntobias@gmail.com>
2023-04-05 13:54:23 +0200

    Add functions

--- nanopub-utils/src/lib/nanopub-utils.ts
+++ nanopub-utils/src/lib/nanopub-utils.ts
@@ -83,6 +83,21 @@ export const getLatestNp = callback => {
     .then(response => response.text())
     .then(data => {
       const lines = data.split(/\n/)
-      callback(lines[lines.length - 2])
+      callback(lines[lines.length - 2].trim())
     })
+}
+
+export const isTrustyUri = (uri : string) => {
+  return /.*[^A-Za-z0-9_\-](RA[A-Za-z0-9_\-]{43})/.test(uri)
+}
+
+export const getArtifactCode = (uri : string) => {
+  if (isTrustyUri(uri)) return uri.replace(/^.*[^A-Za-z0-9_\-](RA[A-Za-z0-9_\-]{43})$/, '$1')
+  return null
+}
+
+export const getShortCode = (uri : string) => {
+  const ac = getArtifactCode(uri)
+  if (ac == null) return null
+  return ac.substring(0, 10)
 }
