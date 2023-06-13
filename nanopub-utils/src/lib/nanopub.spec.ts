bfd65609190f7ae25d6eb27e5e57b8bd8b9262fb
Vincent Emonet <vincent.emonet@gmail.com>
2023-06-13 16:37:38 +0200

    add test

--- nanopub-utils/src/lib/nanopub.spec.ts
+++ nanopub-utils/src/lib/nanopub.spec.ts
@@ -5,4 +5,6 @@ test('fetch', async () => {
   const url = 'http://purl.org/np/RAHtkscyyyJDLvWRuINckQrn5rbHzQKvwakNVC3fmRzGU'
   const np = await Nanopub.fetch(url)
   expect(np.url).toBe(url)
+  expect(np.rdfString.length).toBeGreaterThan(10)
+  expect(Object.keys(np.displayNp).length).toBe(4)
 })
