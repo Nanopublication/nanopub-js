1b6d4c7b57c99aa084fc5d36a2eb0693b208bb61
Tobias Kuhn <kuhntobias@gmail.com>
2023-06-21 10:33:32 +0200

    Adjust test to latest change of getUpdateStatus

--- nanopub-utils/src/lib/nanopub-utils.spec.ts
+++ nanopub-utils/src/lib/nanopub-utils.spec.ts
@@ -3,7 +3,7 @@ import {getUpdateStatus} from './nanopub-utils'
 
 test('getUpdateStatus', async () => {
   const statusList = await getUpdateStatus(npTestUrl)
-  expect(statusList.length).toBeGreaterThan(0)
+  expect(statusList.latestUris.length).toBeGreaterThan(0)
 })
 
 const npTestUrl = 'http://purl.org/np/RAHtkscyyyJDLvWRuINckQrn5rbHzQKvwakNVC3fmRzGU'
