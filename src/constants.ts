5db9ffdae537226cb2f5e4bbb9b13bcb95337570
Virginia Balseiro <info@virginiabalseiro.com>
2026-01-05 07:40:45 +0100

    Minor

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,10 @@
 export const DEFAULT_NANOPUB_URI = 'http://purl.org/nanopub/temp/np/';
+
+export const TEST_NANOPUB_REGISTRY_URL =
+  'https://test.registry.knowledgepixels.com/np/';
+
+export const NANOPUB_REGISTRY_URLS = [
+  'https://registry.petapico.org/np/',
+  'https://registry.knowledgepixels.com/np/',
+  'https://registry.np.trustyuri.net/np/',
+];
