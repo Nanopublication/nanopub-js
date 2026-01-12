82027e0cbc9be0baeb8c79a6d843dd9901f921b3
Virginia Balseiro <info@virginiabalseiro.com>
2026-01-12 07:06:42 +0100

    Mark validate as not implemented

--- src/validate.ts
+++ src/validate.ts
@@ -1,10 +1,5 @@
 import type { Nanopub, ValidationOptions, ValidationResult } from './types';
 
 export async function validate(np: Nanopub, options?: ValidationOptions): Promise<ValidationResult> {
-  // TODO: implement structural, signature, SHACL validation
-  return {
-    structureValid: true,
-    signatureValid: !!np.signature,
-    shaclValid: true
-  };
+ throw new Error("validate() not implemented yet");
 }
