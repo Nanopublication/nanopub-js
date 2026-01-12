80c5d1391ca45321b8cc13c525f44ccd05b50533
Virginia Balseiro <info@virginiabalseiro.com>
2026-01-12 08:00:40 +0100

    Add coverage reports

--- vitest.config.js
+++ vitest.config.js
@@ -7,8 +7,23 @@ export default defineConfig({
     globals: true,
     environment: "node",
     include: ["tests/**/*.test.ts"],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      reportsDirectory: "./coverage",
+      exclude: [
+        "node_modules/**",
+        "dist/**",
+        "tests/**",
+        "src/types",
+        "src/utils/wasm.ts",
+        "src/validate.ts", // TEMP excluding until validate is implemented
+        "*/*.d.ts",
+        "*.config.{ts,js}"
+      ],    },
     deps: {
-      inline: ["@nanopub/sign"], 
+      inline: ["@nanopub/sign", "/\?url$/"], 
+      
     },
     poolOptions: {
       threads: { singleThread: true },
