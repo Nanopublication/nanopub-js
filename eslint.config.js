7e78578d80e6ee331091cbe072586745504096e8
Virginia Balseiro <info@virginiabalseiro.com>
2026-01-13 09:09:14 +0100

    Config linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,16 @@
 import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import prettierConfig from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
+  [globalIgnores([
+    'node_modules/**',
+    'dist/**',
+    'coverage/**',
+    '.yarn/**',
+    '.cache/**',
+  ])],
   eslint.configs.recommended,
   tseslint.configs.recommended,
   prettierConfig,
